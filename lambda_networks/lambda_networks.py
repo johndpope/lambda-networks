c36cbfd91d7f105b3768760444cf6998be1e804e
John D. Pope <jp@bellgeorge.com>
2021-02-27 16:04:48 +1100

    draft

--- lambda_networks/lambda_networks.py
+++ lambda_networks/lambda_networks.py
@@ -1,6 +1,7 @@
 import torch
 from torch import nn, einsum
 from einops import rearrange
+from typing import Optional
 
 # helpers functions
 
@@ -20,29 +21,42 @@ def calc_rel_pos(n):
 # lambda layer
 
 class LambdaLayer(nn.Module):
+    """Lambda layer from `"LambdaNetworks: Modeling long-range interactions without attention"
+    <https://openreview.net/pdf?id=xTJEN-ggl1b>`_.
+    Args:
+        in_channels (int): input channels
+        out_channels (int, optional): output channels
+        dim_k (int): key dimension
+        n (int, optional): number of input pixels
+        r (int, optional): receptive field for relative positional encoding
+        num_heads (int, optional): number of attention heads
+        dim_u (int, optional): intra-depth dimension
+    """
     def __init__(
         self,
         dim,
         *,
-        dim_k,
-        n = None,
-        r = None,
-        heads = 4,
-        dim_out = None,
-        dim_u = 1):
+        dim_k: int,
+        n: Optional[int] = None,
+        r: Optional[int] = None,
+        num_heads: int = 4,
+        dim_out= None,
+        dim_u: int = 1
+    )-> None:
         super().__init__()
         dim_out = default(dim_out, dim)
         self.u = dim_u # intra-depth dimension
-        self.heads = heads
+        self.num_heads = num_heads
 
-        assert (dim_out % heads) == 0, 'values dimension must be divisible by number of heads for multi-head query'
-        dim_v = dim_out // heads
+        assert (dim_out % num_heads) == 0, 'values dimension must be divisible by number of heads for multi-head query'
+        dim_v = dim_out // num_heads
 
-        self.to_q = nn.Conv2d(dim, dim_k * heads, 1, bias = False)
+        # Project input and context to get queries, keys & values
+        self.to_q = nn.Conv2d(dim, dim_k * num_heads, 1, bias = False)
         self.to_k = nn.Conv2d(dim, dim_k * dim_u, 1, bias = False)
         self.to_v = nn.Conv2d(dim, dim_v * dim_u, 1, bias = False)
 
-        self.norm_q = nn.BatchNorm2d(dim_k * heads)
+        self.norm_q = nn.BatchNorm2d(dim_k * num_heads)
         self.norm_v = nn.BatchNorm2d(dim_v * dim_u)
 
         self.local_contexts = exists(r)
@@ -55,25 +69,31 @@ class LambdaLayer(nn.Module):
             self.rel_pos_emb = nn.Parameter(torch.randn(rel_lengths, rel_lengths, dim_k, dim_u))
             self.rel_pos = calc_rel_pos(n)
 
-    def forward(self, x):
-        b, c, hh, ww, u, h = *x.shape, self.u, self.heads
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        b, c, hh, ww, u, h = *x.shape, self.u, self.num_heads
 
+        # Project inputs & context to retrieve queries, keys and values
         q = self.to_q(x)
         k = self.to_k(x)
         v = self.to_v(x)
 
+        # Normalize queries & values
         q = self.norm_q(q)
         v = self.norm_v(v)
 
+        # B x (num_heads * dim_k) * H * W -> B x num_heads x dim_k x (H * W)e
         q = rearrange(q, 'b (h k) hh ww -> b h k (hh ww)', h = h)
         k = rearrange(k, 'b (u k) hh ww -> b u k (hh ww)', u = u)
         v = rearrange(v, 'b (u v) hh ww -> b u v (hh ww)', u = u)
 
+        # Normalized keys
         k = k.softmax(dim=-1)
 
+        # Content function
         λc = einsum('b u k m, b u v m -> b k v', k, v)
         Yc = einsum('b h k n, b k v -> b h v n', q, λc)
 
+        # Position functione
         if self.local_contexts:
             v = rearrange(v, 'b u v (hh ww) -> b u v hh ww', hh = hh, ww = ww)
             λp = self.pos_conv(v)
